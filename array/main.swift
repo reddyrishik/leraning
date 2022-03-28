3ba2820afd998d2db92bb6d456193074e250e703
reddyrishik <bobbytorishik@gmail.com>
2022-03-28 18:24:40 +0530

    array, set

---
+++ array/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  array
+//
+//  Created by Reddy, P on 28/03/22.
+//
+
+import Foundation
+
+var array : [Int] = []
+array.append(3)
+array.append(5)
+array.append(4)
+
+for i in array{
+    print(i)
+}
+print ("total number of items \(array.count)")
+
+var friends: Set = ["rishik","reddy","rohit"]
+print(friends)
+
+for items in friends{
+    print("\(friends)")
+}
+
+
